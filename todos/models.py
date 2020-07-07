679b6e7f991679d0995803e4c611fe24faf79e40
smdmori <mortezasamadi1371@gmail.com>
2020-07-07 15:40:59 +0430

    started todo app

---
+++ todos/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from users.models import CustomUser
+
+# Create your models here.
+class Todo(models.Model):
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
